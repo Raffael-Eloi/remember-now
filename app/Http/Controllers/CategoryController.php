d9b14cca920076256bad80d2988761862d05972a
raffa <raffaeleloi121@gmail.com>
2022-03-28 11:00:48 -0300

    creation of table category

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Category;
+use Illuminate\Http\Request;
+
+class CategoryController extends Controller
+{
+    public function getAllCategories()
+    {
+        $categories = Category::get()->toJson(JSON_PRETTY_PRINT);
+        return response($categories, 200);
+    }
+
+    public function createCategory(Request $request)
+    {
+        $category = new Category;
+        $category->description = $request->description;
+        $category->save();
+
+        return response()->json([
+            "message" => "Category created"
+        ], 201);
+    }
+
+    public function getCategory($id)
+    {
+        if (Category::where('id', $id)->exists()) {
+            $category = Category::where('id', $id)->get()->toJson(JSON_PRETTY_PRINT);
+            return response($category, 200);
+        } else {
+            return response()->json([
+                "message" => "Category not found"
+            ], 404);
+        }
+    }
+
+    public function updateCategory(Request $request, $id)
+    {
+        if (Category::where('id', $id)->exists()) {
+            $category = Category::find($id);
+            $category->description = is_null($request->description) ? $category->description : $request->description;
+            $category->save();
+
+            return response()->json([
+                "message" => "Categoy updated successfully"
+            ], 200);
+        } else {
+            return response()->json([
+                "message" => "Category not fount"
+            ], 404);
+        }
+    }
+
+    public function deleteCategory($id)
+    {
+        if (Category::where('id', $id)->exists()) {
+            $category = Category::find($id);
+            $category->delete();
+
+            return response()->json([
+                "message" => "Category deleted"
+            ], 202);
+        } else {
+            return response()->json([
+                "message" => "Category not found"
+            ], 404);
+        }
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CategoryController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,6 +14,12 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+Route::get('category', [CategoryController::class, 'getAllCategories']);
+Route::get('category/{id}', [CategoryController::class, 'getCategory']);
+Route::post('category', [CategoryController::class, 'createCategory']);
+Route::put('category/{id}', [CategoryController::class, 'updateCategory']);
+Route::delete('category/{id}', [CategoryController::class, 'deleteCategory']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
